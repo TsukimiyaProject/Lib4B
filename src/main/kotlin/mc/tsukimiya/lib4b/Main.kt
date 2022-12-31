1bf53322b2c51a99fc4af84cbad99ebf188fba45
deceitya <49361292+deceitya@users.noreply.github.com>
2023-01-01 03:53:58 +0900

    メッセージコンフィグの追加

--- src/main/kotlin/mc/tsukimiya/lib4b/Main.kt
+++ src/main/kotlin/mc/tsukimiya/lib4b/Main.kt
@@ -1,6 +1,18 @@
 package mc.tsukimiya.lib4b
 
+import mc.tsukimiya.lib4b.lang.MessageFormatter
 import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin() {
+    companion object {
+        lateinit var instance: Main
+    }
+
+    lateinit var formatter: MessageFormatter
+
+    override fun onEnable() {
+        instance = this
+        saveDefaultConfig()
+        formatter = MessageFormatter(config)
+    }
 }
