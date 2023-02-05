f49a5e40a853e915b6f57cc769203bff1b80c911
deceitya <49361292+deceitya@users.noreply.github.com>
2023-02-06 03:11:34 +0900

    正の数自然数の追加

--- src/main/kotlin/mc/tsukimiya/lib4b/command/Validation.kt
+++ src/main/kotlin/mc/tsukimiya/lib4b/command/Validation.kt
@@ -10,10 +10,28 @@ object Validation {
         }
     }
 
+    fun isNaturalNumber(str: String): Boolean {
+        return try {
+            val i = str.toInt()
+            i >= 0
+        } catch (e: NumberFormatException) {
+            false
+        }
+    }
+
     fun isDouble(str: String): Boolean {
         return try {
             str.toDouble()
             true
+        } catch (e: NumberFormatException) {
+            false
+        }
+    }
+
+    fun isPositiveNumber(str: String): Boolean {
+        return try {
+            val d = str.toDouble()
+            d >= 0.0
         } catch (e: NumberFormatException) {
             false
         }
